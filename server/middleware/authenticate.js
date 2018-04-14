b2bce1603f56ce5e7d68a61c508737c38efb32d1
ehosaini <me.hosaini@gmail.com>
2018-04-13 21:57:30 -0400

    Add GET /users/me and authentication middleware

---
+++ server/middleware/authenticate.js
@@ -0,0 +1,26 @@
+const {
+  User
+} = require('./../models/user');
+
+
+var authenticate = (req, res, next) => {
+  var token = req.header('x-auth');
+
+  User.findByToken(token).then((user) => {
+    console.log('logging user', user);
+    if (!user) {
+      return Promise.reject();
+    }
+
+    req.user = user;
+    req.token = token;
+    next();
+
+  }).catch((e) => {
+    res.status(401).send();
+  });
+};
+
+module.exports = {
+  authenticate
+}
--- server/server.js
+++ server/server.js
@@ -26,6 +26,9 @@ const {
   _
 } = require('lodash');
 
+const {
+  authenticate
+} = require('./middleware/authenticate');
 
 
 var app = express();
@@ -140,6 +143,28 @@ app.patch('/todos/:id', (req, res) => {
   }).catch((e) => {
     res.status(404).send();
   })
+});
+
+
+// POST User
+app.post('/users', (req, res) => {
+  var userInfo = _.pick(req.body, ['email', 'password']);
+
+  var user = new User(userInfo);
+
+  user.save().then(() => {
+    return user.generateAuthToken();
+  }).then((token) => {
+    res.header('x-auth', token).send(user);
+  }).catch((e) => {
+    res.status(400).send(e);
+  });
+
+});
+
+
+app.get('/users/me', authenticate, (req, res) => {
+  res.send(req.user);
 });
 
 app.listen(port, () => {
